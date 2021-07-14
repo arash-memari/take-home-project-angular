8014bb1db41df0724a28223174349aa247e86f38
Arash <arash_mem@yahoo.com>
2021-07-14 19:49:54 -0400

    cell9 became reusable component

--- src/app/dataset-page/dataset-table10/body-cell9/body-cell9.component.ts
+++ src/app/dataset-page/dataset-table10/body-cell9/body-cell9.component.ts
@@ -3,14 +3,25 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-body-cell9',
   templateUrl: './body-cell9.component.html',
-  styleUrls: ['./body-cell9.component.scss']
+  styleUrls: ['./body-cell9.component.scss'],
 })
 export class BodyCell9Component implements OnInit {
-
   @Input() icon: string;
-  constructor() { }
+  iconClass = ['fas'];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  iconString(): string[] {
+    // debugger
+
+    if (this.icon === 'view_headline') {
+      this.iconClass[1] = 'bars';
+    } else {
+      this.iconClass[1] = this.icon;
+    }
+
+    return this.iconClass;
   }
-
 }
